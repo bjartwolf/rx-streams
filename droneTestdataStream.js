8f4d7b0b431f30dc3746a4af3289322554cd4012
Bjørn Einar <bjartnes@gmail.com>
2013-06-30 16:14:28 +0200

    cleaning up formatting due to vim on windows is a little hard

--- droneTestdataStream.js
+++ droneTestdataStream.js
@@ -1,8 +1,17 @@
 var events = require('events');
-var toObservable = require('./toObservable');
 var drone = new events.EventEmitter();
 altitude = 0;
 setInterval(function () { altitude += 0.08; drone.emit('navdata', {demo: {altitudeMeters: altitude}});} , 100);
 
-var obsDrone = toObservable.call(drone, 'navdata');
-module.exports = obsDrone;
+var stream = require('stream');
+
+var navDataStream = new stream.Readable(
+  {objectMode: true}); 
+
+navDataStream._read = function () {};
+
+drone.on('navdata', function (chunk) {
+  navDataStream.push(chunk);
+});
+
+module.exports = navDataStream;
--- tryObservableToStream.js
+++ tryObservableToStream.js
@@ -3,10 +3,16 @@ var rx = require('rx');
 var obsDrone = require('./droneTestdataObservable.js');
 
 var rxFunc = function (obs) {
-  return obs.where(function(navdata) { return navdata && navdata.demo && navdata.demo.altitudeMeters;})
-  .select(function(navdata) { return navdata.demo.altitudeMeters;})
+	return obs.
+		where(function(navdata) {
+			return navdata && navdata.demo && navdata.demo.altitudeMeters;
+		}).
+		select(function(navdata) {
+			return navdata.demo.altitudeMeters;
+		})
 };
 
 var testStream = new ObservableToStream(obsDrone, rxFunc);
+
 var Serializer = require('./serializer'); 
 testStream.pipe(new Serializer()).pipe(process.stdout);
