e64675cec20f1dd5cabff719ba51f0ab62c18708
bjartwolf <bjartnes@gmail.com>
2013-06-30 23:31:45 +0200

    fixed some tabbing issues

--- streamToObservable.js
+++ streamToObservable.js
@@ -13,16 +13,16 @@ var StreamToObservable = function(stream) {
     };
     var self = this;
     var throughStream = new streams.PassThrough({objectMode: true});
-	return rx.Observable.create(function(observer) {
-		var handler = function () {
-			observer.onNext(throughStream.read());// object stream should only emit single object
-		};
-		throughStream.addListener('readable', handler);
+    return rx.Observable.create(function(observer) {
+        var handler = function () {
+            observer.onNext(throughStream.read());// object stream should only emit single object
+        };
+        throughStream.addListener('readable', handler);
         stream.pipe(throughStream);
-		return function() {
-			throughStream.removeListener('readable', handler);
+        return function() {
+            throughStream.removeListener('readable', handler);
             stream.unpipe(throughStream);
-		};
-	});
+        };
+    });
 };
 module.exports = StreamToObservable;
