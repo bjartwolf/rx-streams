9debacbd63aef5619699f325c88f5b476dbe7fa9
bjartwolf <bjartnes@gmail.com>
2013-06-30 23:15:19 +0200

    added comments in toObsvarble

--- toObservable.js
+++ toObservable.js
@@ -1,3 +1,5 @@
+// This turns an EventEmitter into an observable
+// Used in this demo to make observable sequences for demodata
 var rx = require('rx');
 var toObservable = function(eventName) {
     if (!eventName) { var eventName = 'data';}
